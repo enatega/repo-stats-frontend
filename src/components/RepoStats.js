1c0541bf034459d03eddd106d9dc3b8c812ac791
waleedahmedhere <ahmedwaleed039@gmail.com>
2025-12-15 17:22:43 +0500

    API base URL updated

--- src/components/RepoStats.js
+++ src/components/RepoStats.js
@@ -12,8 +12,9 @@ import {
 import { CSVLink } from "react-csv";
 import "./RepoStats.css";
 
-const API_BASE = "https://repo-stats-backend-production.up.railway.app/api";
-// const API_BASE = "http://localhost:=3000/api";
+// const API_BASE = "https://repo-stats-backend-production.up.railway.app/api";
+const API_BASE = "http://localhost:4000/api";
+
 
 function RepoStats() {
   const [accounts, setAccounts] = useState([]);
